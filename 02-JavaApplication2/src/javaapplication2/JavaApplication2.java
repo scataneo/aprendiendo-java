0cd79ce8af1964a8cca782eb1915e5ad5c812d3b
sergio <ingresosppc@gmail.com>
2020-09-13 20:39:30 -0300

    While resualto

--- 02-JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ 02-JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -1,14 +1,36 @@
 
 package javaapplication2;
 
-/**
- *
- * @author SergioNicolas
- */
+import java.util.Scanner;
+
 public class JavaApplication2 {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        //Aca importamos la clase scanner para ingresar datos por comando
+        Scanner teclado=new Scanner(System.in);
+        
+        //Declaramos las variables a utilizar
+        int n,x;
+        
+        //Aca inicializacion x
+        x=1;
+        
+        //Usuario ingresa un numero
+        System.out.println("Ingresa un numero");
+        
+        //En la variable n guardamos el numero que ingresamos por consola
+        n=teclado.nextInt();
+        
+        //Mientas sea verdad que la x es menor que el numero que ingresamos se ejecuta el codigo
+        while(x<=n){
+          
+            System.out.println(x);
+            
+            x=x+1;
+        
+        }
+        
     }
     
 }
