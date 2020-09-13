da83775f08dd1bbfdd5d9050db880307d801346b
sergio <ingresosppc@gmail.com>
2020-09-13 18:21:46 -0300

    Aplicacion Java

---
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,61 @@
+
+package javaapplication1;
+
+import java.util.Scanner;
+
+
+public class JavaApplication1 {
+
+    
+    public static void main(String[] args) {
+      
+      Scanner teclado=new Scanner(System.in);
+      
+      int n,x;
+      
+      x=1;
+      
+      System.out.println("Ingrese un numero");
+      
+      n=teclado.nextInt();
+      
+      while(x<=n){
+      
+       x=x+1;
+       
+       
+      System.out.println(x);
+      
+      
+      
+       
+      }
+     
+        
+    }
+    
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
