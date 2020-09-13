5a8bfebe9f6c0e211966ae5eabfd5cc7e32782c8
sergio <ingresosppc@gmail.com>
2020-09-13 20:17:57 -0300

    While

--- 01-JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ 01-JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -1,42 +1,28 @@
 
 package javaapplication1;
 
-import java.util.Scanner;
-
 
 public class JavaApplication1 {
 
     
     public static void main(String[] args) {
       
-      Scanner teclado=new Scanner(System.in);
+      int num=1;
       
-      int n,x;
+      while(num<=100){
       
-      x=1;
+      num=num+1
       
-      System.out.println("Ingrese un numero");
-      
-      n=teclado.nextInt();
-      
-      while(x<=n){
-      
-       x=x+1;
-       
-       
-      System.out.println(x);
-      
-      
-      
-       
+       System.out.println(num);
       }
+       
+     }
      
         
-    }
-    
-    
-    
 }
+    
+    
+ 
 
 
 
